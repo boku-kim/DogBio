ceaa8ac3777aad921d309b5519b2f17bba6f0daf
gyuseek <8ro8rome@gmail.com>
2019-10-08 11:53:21 +0900

    EditPassword & SearchdogInfo v1.0

---
+++ EditPassword/EditPassword/EditPasswordDlg.h
@@ -0,0 +1,45 @@
+﻿
+// EditPasswordDlg.h: 헤더 파일
+#include "CheckPassword.h"
+#include "ChangePassword.h"
+//
+
+#pragma once
+
+
+// CEditPasswordDlg 대화 상자
+class CEditPasswordDlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CEditPasswordDlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+	CheckPassword* checkpw;
+	ChangePassword* changepw;
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_EDITPASSWORD_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnEnChangeEdit1();
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnEnChangeEdit2();
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnEnChangeEdit3();
+	afx_msg void OnBnClickedButton3();
+};
