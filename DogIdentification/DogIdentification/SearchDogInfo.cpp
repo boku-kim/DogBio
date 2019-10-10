d950ba6e36fa38ac9b55bc37729bbc86ca72ffc1
gyuseek <8ro8rome@gmail.com>
2019-10-10 10:49:03 +0900

    Edit password 확인 기능 추가

---
+++ DogIdentification/DogIdentification/SearchDogInfo.cpp
@@ -0,0 +1,87 @@
+﻿// SearchDogInfo.cpp: 구현 파일
+//
+
+#include "stdafx.h"
+#include "DogIdentification.h"
+#include "SearchDogInfo.h"
+#include "afxdialogex.h"
+
+
+// SearchDogInfo 대화 상자
+
+IMPLEMENT_DYNAMIC(SearchDogInfo, CDialogEx)
+
+SearchDogInfo::SearchDogInfo(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_DIALOG_SEARCHDOGINFO, pParent)
+{
+
+}
+
+SearchDogInfo::~SearchDogInfo()
+{
+}
+
+void SearchDogInfo::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_PICDOGIMG, m_PIC);
+}
+
+
+BEGIN_MESSAGE_MAP(SearchDogInfo, CDialogEx)
+	ON_BN_CLICKED(IDC_BTN_LOADIMG, &SearchDogInfo::OnBnClickedBtnLoadimg)
+	ON_BN_CLICKED(IDC_BTN_OPENCAMERA, &SearchDogInfo::OnBnClickedBtnOpencamera)
+	ON_BN_CLICKED(IDC_BTN_SEARCH, &SearchDogInfo::OnBnClickedBtnSearch)
+END_MESSAGE_MAP()
+
+
+// SearchDogInfo 메시지 처리기
+
+
+void SearchDogInfo::OnBnClickedBtnLoadimg()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	CString szFilter = _T(" All Files(*.*)|*.*|");
+	CFileDialog filedlg(TRUE, NULL, NULL, OFN_HIDEREADONLY | OFN_OVERWRITEPROMPT, (LPCTSTR)szFilter);
+	if (filedlg.DoModal() == IDOK)
+	{
+		//picture control 크기 획득
+		CRect rect;
+		m_PIC.GetClientRect(rect);
+		int iwidth, iheight;
+		iwidth = rect.Width();
+		iheight = rect.Height();
+
+		// 이미지 경로 획득
+		CString img_path = filedlg.GetPathName();
+		CDC* pDc;
+		pDc = m_PIC.GetWindowDC();
+		CDC memdc;
+
+		int width, height;
+		m_bmpBitmap.Destroy();
+		m_bmpBitmap.Load(img_path);
+		width = m_bmpBitmap.GetWidth();
+		height = m_bmpBitmap.GetHeight();
+		memdc.CreateCompatibleDC(pDc);
+		m_bmpBitmap.TransparentBlt(pDc->m_hDC, 0, 0, iwidth, iheight, SRCCOPY);
+		m_PIC.ReleaseDC(pDc);
+		pDc->DeleteDC();
+		pDc = NULL;
+	}
+}
+
+
+void SearchDogInfo::OnBnClickedBtnOpencamera()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	CameraMgr* pCameraInst = CameraMgr::GetInstance();
+	pCameraInst->m_picDog = &(this->m_PIC);
+	pCameraInst->DoModal();
+}
+
+
+void SearchDogInfo::OnBnClickedBtnSearch()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+}
---
+++ DogIdentification/DogIdentification/resource.h
@@ -0,0 +1,57 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// DogIdentification.rc에서 사용되고 있습니다.
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_DOGIDENTIFICATION_DIALOG    102
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG_DOGREGISTER          129
+#define IDD_DIALOG_CAMERA               131
+#define IDD_DIALOG_PASSWORD             133
+#define IDD_DIALOG_LOGIN                135
+#define IDD_DIALOG_EDITPASSWORD         137
+#define IDD_DIALOG_CHECKPASSWORD        139
+#define IDD_DIALOG_CHANGEPASSWORD       141
+#define IDD_DIALOG_SEARCHDOGINFO        143
+#define IDD_DIALOG_EDITDOGINFO          145
+#define IDD_DIALOG_MAIN                 147
+#define IDC_EDIT_DOGNAME                1000
+#define IDC_EDIT_DOGAGE                 1001
+#define IDC_EDIT_DOGSPECIES             1002
+#define IDC_EDIT_DOGADDR                1003
+#define IDC_RADIO_MALE                  1004
+#define IDC_RADIO_FEMALE                1005
+#define IDC_BTN_LOADIMG                 1006
+#define IDC_BTN_OPENCAMERA              1007
+#define IDC_BTN_ANALYSIS                1008
+#define IDC_STATIC_GENDER               1009
+#define IDC_PICDOGIMG                   1010
+#define IDC_STATIC_CAMERAPIC            1011
+#define IDC_BTN_TAKE                    1012
+#define IDC_EDIT1                       1013
+#define IDC_EDIT2                       1014
+#define IDC_BTN_LOGIN                   1016
+#define IDC_EDIT_PASSWORD               1018
+#define IDC_EDIT3                       1019
+#define IDC_BTN_CHECK                   1020
+#define IDC_BTN_CHANGE                  1021
+#define IDC_BTN_CANCEL                  1022
+#define IDC_BTN_OK                      1023
+#define IDC_BTN_SEARCH                  1028
+#define IDC_BTN_EDITPW                  1031
+#define IDC_BTN_REGISTERDOGINFO         1032
+#define IDC_BTN_EDITDOGINFO             1033
+#define IDC_BTN_SEARCHDOGINFO           1034
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        149
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1033
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
